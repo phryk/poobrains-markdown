7d520bcbc8694825d0d626374180ec59aabf9fb0
phryk <phryk@commander1024.de>
2017-03-19 04:11:03 +0100

    moved magic reference functionality from pdn into poobrains-markdown

--- example.py
+++ example.py
@@ -6,10 +6,13 @@ import poobrains_markdown
 
 app = poobrains.app
 
+
 @poobrains.app.expose('/article', mode='full')
 class Article(poobrains.auth.Administerable):
 
+    title = poobrains.storage.fields.CharField(null=False)
     text = poobrains_markdown.MarkdownField()
+
 
 if __name__ == '__main__':
     app.run()
--- poobrains_markdown/__init__.py
+++ poobrains_markdown/__init__.py
@@ -3,17 +3,102 @@ import jinja2
 import poobrains
 import markdown
 
+
+def magic_markdown_loader(storable, handle):
+
+    storables = poobrains.storage.Storable.children_keyed()
+    for k, v in storables.iteritems():
+        storables[k.lower()] = v # Allows us to use the correct case, or just lowercase
+
+    cls = storables[storable]
+    instance = cls.load(handle)
+
+
+class MagicDict(dict):
+
+    """ Magical dict to (try to) generate links on demand """
+
+    loader = None
+
+    def __init__(self, *args, **kwargs):
+
+        super(MagicDict, self).__init__(*args, **kwargs)
+        self.set_loader(magic_markdown_loader)
+
+
+    def __contains__(self, key):
+        import pudb; pudb.set_trace()
+        if not super(MagicDict, self).__contains__(key):
+
+            if self._valid_magickey(key):
+                
+                storable, handle = key.split('/')
+                try:
+                    return bool(self.loader(storable, handle))
+                except:
+                    return False
+
+            return False
+
+        return True
+    
+    
+    def __getitem__(self, key):
+
+        if not super(MagicDict, self).__contains__(key):
+
+            if self._valid_magickey(key):
+
+                storable, handle = key.split('/')
+                try:
+                    instance = self.loader(storable, handle)
+
+                    try:
+                        url = instance.url('full')
+                    except:
+
+                        try:
+                            url = instance.url('teaser')
+                        except:
+                            url = "NOLINK" # FIXME: What's the more elegant version of this, again?
+
+                    return (url, instance.title if hasattr(instance, 'title') else None)
+
+                except:
+                    raise KeyError("Couldn't load '%s.%s'." % storable, handle)
+
+
+        return super(MagicDict, self).__getitem__(key)
+
+
+    def _valid_magickey(self, key):
+
+        return '/' in key and len(key.split('/')) == 2
+
+
+    def set_loader(self, loader):
+        self.loader = loader
+
+
+
+class pooMarkdown(markdown.Markdown):
+
+    def __init__(self, *args, **kwargs):
+        
+        super(pooMarkdown, self).__init__(*args, **kwargs)
+        self.references = MagicDict()
+
+
 try:
 
     import config
     if hasattr(config, 'MARKDOWN_CLASS') and issubclass(config.MARKDOWN_CLASS, markdown.Markdown):
         cls = config.MARKDOWN_CLASS
     else:
-        cls = markdown.Markdown
+        cls = pooMarkdown
 
 except ImportError:
-    cls = markdown.Markdown
-
+    cls = pooMarkdown
 
 md = cls(output_format="html5") # means we can globally register markdown extensions with "poobrains_markdown.md.registerExtensions
 
